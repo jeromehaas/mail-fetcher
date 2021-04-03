9d1537ece7fed594750aa88eafb7ce2662dfcb97
Jerome Haas <j.haas@cyberplanet.ch>
2021-04-03 17:05:36 +0200

    Implement: get sender and time of specific mail

--- dashboard.php
+++ dashboard.php
@@ -1,41 +1,77 @@
-<!DOCTYPE html>
-<html lang="en">
 
 <!-------------------------------------------------------------------------------------------------------------------------------------------------------->
-<!--  LOGIN SCRIPT -->
+<!--  MAIL SCRIPTS -->
 <!-------------------------------------------------------------------------------------------------------------------------------------------------------->
 <?php
-if (isset($_POST['login-submit'])) {
-
-  $errorArray = array();
-
-  $email = htmlentities($_POST['email']);
-  $password = htmlentities($_POST['password']);
   
-  if (empty($email) || empty($password)) {
-    $_SESSION['message'] = 'please provide email and username';
-  } else {
-    header("Location: http://localhost:8888/mail-fetcher/dashboard.php");
+  include('./environment.php');
+
+  function openImapConnection($host, $user, $password) {
+    $imap = imap_open($host, $user, $password);
+    echo ($imap ? 'connection successfull' : 'connection failed');
+    return $imap;
   }
 
-}
+  function printMailContentHTML($imap, $id) {
+    $header=imap_headerinfo($imap, $id);
+    $message = (imap_fetchbody($imap,$id, 2));
+    print(quoted_printable_decode($message)."<br>");  
+  };
+
+  function printMailList() {
+
+  };
+
+  function getSenderFromSpecificMail($headerInfo) {
+    
+  }
+
+  function getTimeFromSpecificMail($headerInfo) {
+    $date = strtotime($headerInfo->date);
+    echo date("Y.m.d  -  h:i:s", $date);
+  }
+
+  $imap = openImapConnection($host, $user, $password);
+  $headers = imap_headers($imap);
+  $headerInfo = imap_headerinfo ( $imap , 8 , $from_length = 0 , $subject_length = 0);
+  $totalNumberOfMessages = imap_num_msg($imap);
+  $folders = imap_list($imap, "{login-21.loginserver.ch}", "*");
+  $emailData = imap_search($imap, '')
+
+
+  
+
 ?>
 <!-------------------------------------------------------------------------------------------------------------------------------------------------------->
 <!--  PAGE CONTENT -->
 <!-------------------------------------------------------------------------------------------------------------------------------------------------------->
 
+<!DOCTYPE html>
+  <html lang="en">
+
   <?php include('./partials/head.php'); ?>
   <body>
-  <div id="page-wrapper">
-  
-  <?php include('./partials/header.php'); ?>
-  <div id="dashboard-wrapper">
+    <div id="page-wrapper">
+      
+    <?php include('./partials/header.php'); ?>
+    <div id="dashboard-wrapper">
 
-    <div class="mail-list">mail list</div>
-    <div class="mail-meta">mail list</div>
-    <div class="mail-content">mail list</div>
-  </div>
+      <div class="mail-list">
+        <?php printMailList($headers); ?>
+      </div>
+      <div class="mail-meta">
+        <?php 
+          // print_r($headerInfo->sender);
+          echo $headerInfo->sender[0]->personal . "<br />";
+          // echo $headerInfo->date . "\n";
+          getTimeFromSpecificMail($headerInfo);
+        ?>
+      </div>
+      <div class="mail-content">
+        <?php // printMailContentHTML($imap, 8); ?>
+      </div>
+    </div>
 
-  </div>
+    </div>
   </body>
 </html>
