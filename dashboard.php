c2bee51b082a945d01dbee57ab120f3453e3d8f7
Jerome Haas <j.haas@cyberplanet.ch>
2021-04-05 17:19:16 +0200

    Documentation: write some comments

--- dashboard.php
+++ dashboard.php
@@ -4,6 +4,9 @@
 <!-------------------------------------------------------------------------------------------------------------------------------------------------------->
 <?php
   
+  // START SESSION - CHECK IF SESSION IS ACTIVE
+  // Y: SAVE CREDENTIALS IN VAR
+  // N: REDIRECT TO LOGIN PAGE
   session_start();
   if (isset($_SESSION['host'], $_SESSION['username'], $_SESSION['password'])) {
     $host = $_SESSION['host'];
@@ -14,24 +17,28 @@
     header("Location: http://localhost:8888/mail-fetcher/");
   };
 
+  // OPEN IMAP CONNECTION
   function openImapConnection($host, $username, $password) {
     $imap = imap_open($host, $username, $password);
     return $imap;
   }
 
+  // FORMAT TIME TO READABLE STRING
   function formatTime($date) {
     $date = strtotime($date);
     return date("Y.m.d  -  h:i:s", $date);
   }
- 
+  
+  // GET MAIL CONTENT
   function printMailContentHTML($imap, $id) {
     $header=imap_headerinfo($imap, $id);
     $message = (imap_fetchbody($imap,$id, 2));
     print(quoted_printable_decode($message));  
   };
 
-  function printMailList($imap) {
-    for ($i = 1; $i <= 100; $i++) {
+  // PRINT THE MAIL LIST
+  function printMailList($imap, $totalNumberOfMessages) {
+    for ($i = 1; $i <= $totalNumberOfMessages; $i++) {
       $headerInfo = imap_headerinfo($imap, $i);
       echo '
       <a class="dashboard__mail-list__anchor" href="http://localhost:8888/mail-fetcher/dashboard.php?currentMail=' . $i . '">
@@ -43,6 +50,7 @@
     }
   };
 
+  // PRINT META MAIL INFORMATIONS
   function printMailMeta($headerInfo) {
     echo '
     <img class="dashboard__mail-meta__img" src="./media/icons/avatar.svg" alt="Avatar">
@@ -51,6 +59,7 @@
       <p class="dashboard__mail-meta__date">' . formatTime($headerInfo->date) .  '</p>
     </div>';
   }
+
 
   $imap = openImapConnection($host, $username, $password);
   $headerInfo = imap_headerinfo( $imap , $_GET['currentMail'] ?? 1);
@@ -66,30 +75,23 @@
 
 <!DOCTYPE html>
   <html lang="en">
-
   <?php include('./partials/head.php'); ?>
   <body>
     <div id="page-wrapper">
-      
     <?php include('./partials/header.php'); ?>
     <div id="dashboard__wrapper">
-
       <div class="dashboard__mail-list">
         <ul class="dashboard__mail-list__list">
-          <?php printMailList($imap); ?>
+          <?php printMailList($imap, $totalNumberOfMessages); ?>
         </ul>
       </div>
-
       <div class="dashboard__mail-meta">
         <?php printMailMeta($headerInfo); ?>
       </div>       
-         
       <div class="dashboard__mail-content">
         <?php printMailContentHTML($imap, $currentMail); ?>
       </div>
-
     </div>
-
     </div>
   </body>
 </html>
--- index.php
+++ index.php
@@ -5,11 +5,14 @@
 <!-------------------------------------------------------------------------------------------------------------------------------------------------------->
 <?php
 
+// STAR SESSION AND SET LOGGED IN FLAG PER DEFAULT TO FALSE
 session_start();
 $_SESSION['loggedIn'] = false;
 
+// AFTER THE SUBMIT BUTTON IS CLICKED
 if (isset($_POST['login-submit'])) {
 
+  // CLEAN THE INPUT AND SAVE IN VARS
   $host = htmlentities($_POST['host']);
   $host = '{' . $host . '}';
   $username = htmlentities($_POST['username']);  
@@ -17,16 +20,20 @@ if (isset($_POST['login-submit'])) {
   $imap = imap_open($host, $username, $password);
   $mailboxheaders = imap_headers($mailbox);
 
-  if (empty($username) || empty($password)) {
+  // SHOW ERROR MESSAGE IF NOT ALL INPUT IS PROVIDED 
+  if (empty($username) || empty($password) || empty($host)) {
     $_SESSION['message'] = 'Bitte geben Sie Host, Benutzernamen und Passowrt Ihres Postfachs an.';
+
+  // SHOW ERROR MESSAGE ID CREDENTIALS ARE NOT CORRECT
   } else if ($imap == false) {
     $_SESSION['message'] = 'Die angegebenen Zugangsdaten sind nicht korrekt.';
+
+  // IF INPUT IS CORRECT, STORE CREDENTIALS AS SESSINO AND REDIRECT TO THE DASHBOARD
   } else {
     $_SESSION['host'] = $host;
     $_SESSION['username'] = $username;
     $_SESSION['password'] = $password;
     header("Location: http://localhost:8888/mail-fetcher/dashboard.php");
-
   }
 
 }
@@ -39,20 +46,16 @@ if (isset($_POST['login-submit'])) {
   <?php include('./partials/head.php'); ?>
   <body>
   <div id="page-wrapper">
-  
   <?php include('./partials/header.php'); ?>
-
     <form id="login-form" action="index.php" method="POST">
       <label class="login-form__label" for="email">Host:</label>
-      <input class="login-form__input" name="host" type="text" placeholder="Bitte fügen Sie hier Ihre E-Mail Addresse ein...">
+      <input class="login-form__input" name="host" type="text" placeholder="Bitte fügen Sie hier den Hostname ein...">
       <label class="login-form__label" for="email">Benutzername:</label>
       <input class="login-form__input" name="username" type="text" placeholder="Bitte fügen Sie hier Ihren Benutzernamen ein...">
       <label class="login-form__label" for="password">Passwort:</label>
-      <input class="login-form__input" name="password" type="text" placeholder="Bitte fügen Sie hier Ihr Passwort ein...">
+      <input class="login-form__input" name="password" type="password" placeholder="Bitte fügen Sie hier Ihr Passwort ein...">
       <input class="login-form__submit" name="login-submit" type="submit" value="Einloggen">
     </form>
-
-
   <div id="notifier">
     <?php
       if(isset($_SESSION['message'])) {
