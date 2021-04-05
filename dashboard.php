29428a76ab707ee7c3745eea4a983f62da16a8f2
Jerome Haas <j.haas@cyberplanet.ch>
2021-04-05 11:23:51 +0200

    Implement: style dashboard page

--- dashboard.php
+++ dashboard.php
@@ -12,42 +12,47 @@
     return $imap;
   }
 
+  function formatTime($date) {
+    $date = strtotime($date);
+    return date("Y.m.d  -  h:i:s", $date);
+  }
+ 
   function printMailContentHTML($imap, $id) {
     $header=imap_headerinfo($imap, $id);
     $message = (imap_fetchbody($imap,$id, 2));
-    print(quoted_printable_decode($message)."<br>");  
+    print(quoted_printable_decode($message));  
   };
 
   function printMailList($imap) {
     for ($i = 1; $i <= 100; $i++) {
-      $headerInfo = imap_header($imap, $i);   
-      echo $headerInfo->from[0]->personal . "<br />";
-      echo date("Y.m.d  -  h:i:s", strtotime($headerInfo->date)) . "<br /><br />";
+      $headerInfo = imap_headerinfo($imap, $i);
+      echo '
+      <a class="dashboard__mail-list__anchor" href="http://localhost:8888/mail-fetcher/dashboard.php?currentMail=' . $i . '">
+        <li class="dashboard__mail-list__list-item">
+          <p class="dashboard__mail-list__sender">' . $headerInfo->from[0]->personal . '</p>
+          <p class="dashboard__mail-list__date">' . formatTime($headerInfo->date) . '</p>
+        </li>
+      </a>';
     }
   };
 
-
-  function getTimeFromSpecificMail($headerInfo) {
-    $date = strtotime($headerInfo->date);
-    echo date("Y.m.d  -  h:i:s", $date) . "<br />";
-  }
-  
-  function getSenderNameFromSpecificMail($headerInfo) {
-    echo $headerInfo->sender[0]->personal  . "<br />";
+  function printMailMeta($headerInfo) {
+    echo '
+    <img class="dashboard__mail-meta__img" src="./media/icons/avatar.svg" alt="Avatar">
+    <div class="dashboard__mail-meta__text">          
+      <p class="dashboard__mail-meta__sender">' . $headerInfo->sender[0]->personal . '</p>
+      <p class="dashboard__mail-meta__date">' . formatTime($headerInfo->date) .  '</p>
+    </div>';
   }
 
   $imap = openImapConnection($host, $user, $password);
-  $header = imap_header($imap, 100);
-  $headerInfo = imap_headerinfo ( $imap , 8 , $from_length = 10 , $subject_length = 10);
+  $headerInfo = imap_headerinfo( $imap , $_GET['currentMail'] ?? 1);
   $totalNumberOfMessages = imap_num_msg($imap);
   $folders = imap_list($imap, "{login-21.loginserver.ch}", "*");
   $emailData = imap_search($imap, '');
   $currentMail = $_GET['currentMail'] ?? 1;
-
-
-  
-
 ?>
+
 <!-------------------------------------------------------------------------------------------------------------------------------------------------------->
 <!--  PAGE CONTENT -->
 <!-------------------------------------------------------------------------------------------------------------------------------------------------------->
@@ -57,24 +62,29 @@
 
   <?php include('./partials/head.php'); ?>
   <body>
+    <div class="dashboard__status-bar">
+      success
+    </div>
     <div id="page-wrapper">
       
+
     <?php include('./partials/header.php'); ?>
     <div id="dashboard__wrapper">
 
       <div class="dashboard__mail-list">
-        <?php printMailList($imap); ?>
+        <ul class="dashboard__mail-list__list">
+          <?php printMailList($imap); ?>
+        </ul>
       </div>
+
       <div class="dashboard__mail-meta">
-        <img class="dashboard__mail-meta__img" src="./media/icons/avatar.svg" alt="Avatar">
-        <div class="dashboard__mail-meta__text">
-          <?php getSenderNameFromSpecificMail($headerInfo); ?>
-          <?php getTimeFromSpecificMail($headerInfo); ?>
-        </div>
-      </div>
+        <?php printMailMeta($headerInfo); ?>
+      </div>       
+         
       <div class="dashboard__mail-content">
         <?php printMailContentHTML($imap, $currentMail); ?>
       </div>
+
     </div>
 
     </div>
