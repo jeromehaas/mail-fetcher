bd3ca6a874e109e5e506109a3fa97da1b545b02a
Jerome Haas <j.haas@cyberplanet.ch>
2021-04-05 12:50:37 +0200

    Refactor: connect the login pages and the dashboard with sessions

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,8 @@
 <header id="header">
   <h1 class="header__logo">MailFetcher</h1>
+  <?php
+  if ($_SESSION['loggedIn']) {
+   echo '<a class="header__logout" href="http://localhost:8888/mail-fetcher/">Logout</a>';
+  }
+  ?>
 </header>
